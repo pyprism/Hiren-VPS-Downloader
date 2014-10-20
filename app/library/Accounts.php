2e3b120bc1b19b08bd033c4391610d1f2b90ec9f
pyprism <git.pyprism@gmail.com>
2014-10-20 12:57:18 +0600

    added initial custom class

---
+++ app/library/Accounts.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: prism
+ * Date: 10/20/14
+ * Time: 11:06 AM
+ */
+
+namespace app\library;
+
+class Accounts {
+
+    public function __construct($username, $password){
+
+    }
+}
