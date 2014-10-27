0cd5e0fa0651442e71b1c87bf48d521c46baf1e3
pyprism <git.pyprism@gmail.com>
2014-10-27 20:42:37 +0600

    added basic conf

--- public/src/hiren.js
+++ public/src/hiren.js
@@ -1,3 +1,16 @@
 /**
  * Created by prism on 10/27/14.
  */
+angular.module('hiren', ['ngRoute']).
+    config( function($routeProvider, $locationProvider){
+        $routeProvider
+            .when('/ssh', {
+                templateUrl: '../views/ssh.html',
+                controller: 'ssh'
+
+            });
+        $locationProvider.html5Mode(true);
+    }).
+    controller('ssh', function($scope){
+        $scope.xox = "Sa";
+    });
