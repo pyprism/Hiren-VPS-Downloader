1699f158f626ac1a3280ca6eb6cbd82c72cf85f1
pyprism <git.pyprism@gmail.com>
2014-10-27 20:43:36 +0600

    added basic express

--- server.js
+++ server.js
@@ -1,3 +1,12 @@
 /**
  * Created by prism on 10/27/14.
  */
+var express = require('express'),
+    app = express();
+
+app
+    .use(express.static('./public'))
+    .get('*', function(req, res){
+        res.sendfile('./public/main.html');
+    })
+    .listen(3000);
