238bdf8b70d15c7acbeb957d764c7d28923032b2
pyprism <git.pyprism@gmail.com>
2014-10-27 20:41:11 +0600

    just some test

---
+++ ssh.js
@@ -0,0 +1,9 @@
+/**
+ * Created by prism on 10/27/14.
+ */
+var sys = require('sys');
+var exec = require('child_process').exec;
+
+exec("ufw app list", function(error, stdout, stderr) {
+    sys.puts(stdout)
+});
